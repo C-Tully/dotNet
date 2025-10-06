247bdd28f738a1bba9b59482fa2f0c157a9d9c89
Chris Tully <christully12@gmail.com>
2025-10-06 13:48:44 -0400

    Resolved CORS issue for .Net

--- CEMS.Api/Program.cs
+++ CEMS.Api/Program.cs
@@ -11,10 +11,23 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    //Add services to Container.
+//Add services to Container.
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:8080")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });    
+});
+
 
 var app = builder.Build();
+
+//Use CORS
+app.UseCors("AllowVueDev");
 
 
 
